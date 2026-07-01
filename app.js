c9a462689c14d166f9f18474d1e52ec65143339d
helenebacca-droid <helenebacca@gmail.com>
2026-07-01 10:04:07 -1000

    Add files via upload

--- app.js
+++ app.js
@@ -399,6 +399,7 @@ function savePrices() {
 }
 
 function uid() {
+  if (crypto?.randomUUID) return crypto.randomUUID();
   return `${Date.now().toString(36)}-${Math.random().toString(36).slice(2, 8)}`;
 }
 
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mata-reva-booking-v17";
+const CACHE_NAME = "mata-reva-booking-v18";
 const ASSETS = [
   "./",
   "./index.html",
